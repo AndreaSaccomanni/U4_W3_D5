c4d7a0fe7fe13cc2cc83024333b8a324f23c2498
Andrea Saccomanni <Andrea Saccomanni>
2025-01-24 19:50:43 +0100

    oggetti con findBy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -23,40 +23,42 @@ public class Main {
 
         // ---- CREAZIONE E SALVATAGGIO DI ARTICOLI NEL CATALOGO ----
 
+        // dopo aver istanziato i vari  articoli, e salvati nel database, con il metodo findById salvo dento i vari oggetti, le istanze gia presenti nel DB
+
         Articolo libro1 = articoloDAO.findById(19);
-        Articolo libro2 = new Libro(789012, "1984", 1949, 328, "George Orwell", "Distopia");
-        Articolo libro3 = new Libro(345678, "La Divina Commedia", 1320, 1000, "Dante Alighieri", "Poesia");
+        Articolo libro2 = articoloDAO.findById(21);
+        Articolo libro3 = articoloDAO.findById(22);
 
 //        articoloDAO.save(libro1);
 //        articoloDAO.save(libro2);
 //        articoloDAO.save(libro3);
 
 
-        Rivista rivista1 = new Rivista(111222, "Time", 2023, 120, Periodicita.SETTIMANALE);
-        Rivista rivista2 = new Rivista(333444, "National Geographic", 2022, 100, Periodicita.MENSILE);
-        Rivista rivista3 = new Rivista(555666, "L'Officiel", 2021, 150, Periodicita.SEMESTRALE);
+        Articolo rivista1 = articoloDAO.findById(16);
+        Articolo rivista2 = articoloDAO.findById(17);
+        Articolo rivista3 = articoloDAO.findById(18);
 //        articoloDAO.save(rivista1);
 //        articoloDAO.save(rivista2);
 //        articoloDAO.save(rivista3);
 
-        //----CREAZIONE E SALVATAGGIO DI UTENTI E PRESTITI-----
 
         Utente utente1 = utenteDAO.findById(1);
-        Utente utente2 = new Utente("Luigi", "Bianchi", LocalDate.of(1990, 8, 20), "TESSERA002");
-        Utente utente3 = new Utente("Giulia", "Verdi", LocalDate.of(1995, 12, 10), "TESSERA003");
+        Utente utente2 = utenteDAO.findById(2);
+        Utente utente3 = utenteDAO.findById(3);
 
-        //utenteDAO.save(utente1);
+//        utenteDAO.save(utente1);
 //        utenteDAO.save(utente2);
 //        utenteDAO.save(utente3);
 
+//        creazione di prestiti con elementi gia presenti nel database
 
         Prestito prestito1 = new Prestito(utente1, libro1, LocalDate.of(2025, 1, 20), LocalDate.of(2025, 2, 20), null);
         Prestito prestito2 = new Prestito(utente2, libro2, LocalDate.of(2025, 1, 22), LocalDate.of(2025, 2, 22), null);
         Prestito prestito3 = new Prestito(utente3, libro3, LocalDate.of(2025, 1, 25), LocalDate.of(2025, 2, 25), null);
 
 //        prestitoDAO.save(prestito1);
-//        prestitoDAO.save(prestito2);
-//        prestitoDAO.save(prestito3);
+        prestitoDAO.save(prestito2);
+        prestitoDAO.save(prestito3);
 
         //---- APPLICAZIONE DEI METODI -----
 
@@ -71,7 +73,7 @@ public class Main {
         if (articoloTrovatoConISBN != null) {
             System.out.println("Articolo trovato tramite ISBN: " + articoloTrovatoConISBN);
         } else {
-            System.out.println("Articolo non trovato.");
+            System.out.println("Articolo non trovato!");
         }
 
         //RICERCA TRAMITE ANNO DI PUBBLICAZIONE
@@ -80,7 +82,7 @@ public class Main {
             System.out.println("Articoli trovati tramite l'anno di pubblicazione: ");
             articoliTrovatiConAnnoPubblicazione.forEach(System.out::println);
         } else {
-            System.out.println("Articolo non trovato.");
+            System.out.println("Articolo non trovato!");
         }
 
         //RICERCA TRAMITE AUTORE
@@ -89,7 +91,15 @@ public class Main {
             System.out.println("Libri trovati tramite autore: ");
             articoliTrovatiTramiteAutore.forEach(System.out::println);
         } else {
-            System.out.println("Nessun libro trovato.");
+            System.out.println("Nessun libro trovato!");
+        }
+
+        //RICERCA TRAMITE TITOLO O PARTE DI ESSO
+        List<Articolo> articoliTrovatiTramiteTitolo = articoloDAO.ricercaTramiteTitolo("Java");
+        if (articoliTrovatiTramiteTitolo != null) {
+            articoliTrovatiTramiteTitolo.forEach(System.out::println);
+        } else {
+            System.out.println("Nessun articolo trovato!");
         }
 
 
--- src/main/java/org/example/dao/ArticoloDAO.java
+++ src/main/java/org/example/dao/ArticoloDAO.java
@@ -29,6 +29,7 @@ public class ArticoloDAO {
             System.out.println(e.getMessage());
         }
     }
+
     //metodo per ceracre un articolo nel catalogo tramite id
     public Articolo findById(int id) {
         Articolo found = em.find(Articolo.class, id);
@@ -36,6 +37,7 @@ public class ArticoloDAO {
 
         return found;
     }
+
     //metodo per rimuovere un articolo dal catalogo tramite id
     public void remove(int codiceISBN) {
         try {
@@ -53,31 +55,32 @@ public class ArticoloDAO {
     }
 
     // metoto per cercare un articolo tramite codice ISBN
-    public Articolo findByISBN(int codiceISBN){
-       Query q = em.createQuery("SELECT a FROM Articolo a WHERE a.ISBN = :isbn");
+    public Articolo findByISBN(int codiceISBN) {
+        Query q = em.createQuery("SELECT a FROM Articolo a WHERE a.ISBN = :isbn");
         q.setParameter("isbn", codiceISBN);
         return (Articolo) q.getSingleResult();
     }
 
     //metodo per cercare articolo tramite anno di pubblicazione
-    public List<Articolo> findByAnnoPubblicazione(int annoPubblicazione){
+    public List<Articolo> findByAnnoPubblicazione(int annoPubblicazione) {
         Query q = em.createQuery("SELECT a FROM Articolo a WHERE a.annoPubblicazione = :annoPubblicazione");
         q.setParameter("annoPubblicazione", annoPubblicazione);
         return q.getResultList();
     }
 
     //metodo per cercare tramite autore
-    public List<Libro> ricercaTramiteAutore(String autore){
+    public List<Libro> ricercaTramiteAutore(String autore) {
         Query q = em.createQuery("SELECT a FROM Articolo a WHERE a.autore = :autore");
-        q.setParameter("autore", autore);
+        q.setParameter("autore",   autore );
         return q.getResultList();
     }
 
-//    //metodo per cercare tramite titolo o parte di esso
-//public Articolo ricercaTramiteTitolo(String titolo) {
-//
-//}
-
+    //metodo per cercare tramite titolo o parte di esso
+    public List<Articolo> ricercaTramiteTitolo(String titolo) {
+        Query q = em.createQuery("SELECT a FROM Articolo a WHERE a.titolo LIKE :titolo");
+        q.setParameter("titolo", "%" +  titolo + "%");
+        return q.getResultList();
+    }
 
 
 }
